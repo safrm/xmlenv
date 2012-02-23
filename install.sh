620561eb8828dc7645ea3c1d621cbeae3f87cd4b
Miroslav Safr <Miroslav.Safr@gmail.com>
2012-02-23 10:16:36 +0200

    renamed xmlenv.sh to xmlenv

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ DATADIR=/usr/local/share
 
 sudo mkdir -p -m 0755 $BINDIR
 sudo mkdir -p -m 0755 $DATADIR/xmlenv/
-sudo install -m 0777 -v ./xmlenv.sh  $BINDIR/
+sudo install -m 0777 -v ./xmlenv  $BINDIR/
 sudo install -m 0664 -v ./compare-pkgs.xsl $DATADIR/xmlenv/
